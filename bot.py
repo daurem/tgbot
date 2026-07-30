79f8c4dc7b8aba3f0c2ad719982000e50aaa33ed
ayzon <daureeen1@gmail.com>
2026-07-30 20:13:05 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -343,7 +343,7 @@ async def start_keepalive_server():
 
 async def main():
     await start_keepalive_server()
-    await dp.start_polling(bot)
+    await dp.await bot.delete_webhook(drop_pending_updates=True)start_polling(bot)
 
 
 if __name__ == "__main__":
