6c2c67f86937247e9e59c6f17e520686724e353d
ayzon <daureeen1@gmail.com>
2026-07-31 18:25:30 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,6 @@ from aiogram.types import (
 )
 from aiohttp import web
 import yt_dlp
-import bgutil_ytdlp_pot_provider  # это зарегистрирует плагин в yt-dlp
 
 # ==== ЯВНО ПОДКЛЮЧАЕМ ПЛАГИН ДЛЯ PO-TOKEN ====
 # Это гарантирует, что yt-dlp его увидит и зарегистрирует
