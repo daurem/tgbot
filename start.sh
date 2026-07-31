061009f5397126995622ec82fb8c7d5cb91e1398
ayzon <daureeen1@gmail.com>
2026-07-31 18:17:31 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ node /opt/bgutil-provider/server/build/main.js &
 BGUTIL_PID=$!
 
 # Небольшая пауза, чтобы сервер успел подняться до первого запроса к YouTube
-sleep 3
+sleep 5
 
 if kill -0 "$BGUTIL_PID" 2>/dev/null; then
     echo "[start.sh] PO-Token провайдер запущен (pid $BGUTIL_PID)"
